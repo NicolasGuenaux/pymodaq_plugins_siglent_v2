c3249cf395e3ab9b9f03ccd72bc561078c6a2ebb
NicolasGuenaux <nicolas.guenaux@cea.fr>
2025-02-06 22:06:32 +0100

    updated the main files to new pymodaq
    prise en compte de la nouvelle version de pymodaq. Dans ini_stage, je ne traite pas le cas où on est slave.. je ne sais pas si c'est un pb : je n'ai pas refait exactement la même chose que dans l'ancienne version non plus...

--- src/pymodaq_plugins_siglent/daq_move_plugins/daq_move_Siglent.py
+++ src/pymodaq_plugins_siglent/daq_move_plugins/daq_move_Siglent.py
@@ -5,10 +5,10 @@ from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_pa
 from pymodaq.utils.daq_utils import ThreadCommand # object used to send info back to the main thread
 from pymodaq.utils.parameter import Parameter
 
+import sys, os
+from siglent_wrapper import ActuatorWrapper
+sys.path.append('c:\\Users\\attose1_VMI\\local_repository\\pymodaq_plugins_siglent\\src\\pymodaq_plugins_siglent\\daq_move_plugins')
 
-class PythonWrapperOfYourInstrument:
-    #  TODO Replace this fake class with the import of the real python wrapper of your instrument
-    pass
 
 # TODO:
 # (1) change the name of the following class to DAQ_Move_TheNameOfYourChoice
@@ -16,7 +16,7 @@ class PythonWrapperOfYourInstrument:
 #     for the class name and the file name.)
 # (3) this file should then be put into the right folder, namely IN THE FOLDER OF THE PLUGIN YOU ARE DEVELOPING:
 #     pymodaq_plugins_my_plugin/daq_move_plugins
-class DAQ_Move_Template(DAQ_Move_base):
+class DAQ_Move_Siglent(DAQ_Move_base):
     """ Instrument plugin class for an actuator.
     
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
@@ -38,26 +38,37 @@ class DAQ_Move_Template(DAQ_Move_base):
     # TODO add your particular attributes here if any
 
     """
-    is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
-    _axis_names: Union[List[str], Dict[str, int]] = ['Axis1', 'Axis2']  # TODO for your plugin: complete the list
-    _controller_units: Union[str, List[str]] = 'mm'  # TODO for your plugin: put the correct unit here, it could be
+    is_multiaxes = True  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
+    _axis_names: Union[List[str], Dict[str, int]] = ['Amplitude', 'Phase', 'Frequency', 'Delay']  # TODO for your plugin: complete the list
+    _controller_units: Union[str, List[str]] = ['V', 'deg', 'Hz', 's']  # TODO for your plugin: put the correct unit here, it could be
     # TODO  a single str (the same one is applied to all axes) or a list of str (as much as the number of axes)
-    _epsilon: Union[float, List[float]] = 0.1  # TODO replace this by a value that is correct depending on your controller
+    _epsilon: Union[float, List[float]] = [0.1, 0.1, 0.1, 0.1]  # TODO replace this by a value that is correct depending on your controller
     # TODO it could be a single float of a list of float (as much as the number of axes)
     data_actuator_type = DataActuatorType.DataActuator  # wether you use the new data style for actuator otherwise set this
     # as  DataActuatorType.float  (or entirely remove the line)
 
-    params = [   # TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
+    params = [
+        {'title': 'Burst:', 'name': 'burst', 'type': 'text', 'value': "ON"},
+        {'title': 'Frequency:', 'name': 'frequency', 'type': 'float', 'value': 1e7},
+        {'title': 'Offset:', 'name': 'offset', 'type': 'float', 'value': 0},
+        {'title': 'Delay:', 'name': 'delay', 'type': 'float', 'value': 5e-6},
+        {'title': 'Rep number:', 'name': 'cycles', 'type': 'int', 'value': 1},
+        # {'title': 'Wavetype:', 'name': 'wavetype', 'type': 'text', 'value': "SINE"},
+        {'title': 'Wavetype:', 'name': 'wavetype', 'type': 'itemselect',
+         'value': dict(all_items=["ARB", "SINE", "RAMP", "SQUARE", "DC"], selected=["SINE"])},
+        {'title': 'File:', 'name': 'file', 'type': 'text', 'value': ""},
+         # TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
                 ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
     # _epsilon is the initial default value for the epsilon parameter allowing pymodaq to know if the controller reached
     # the target value. It is the developer responsibility to put here a meaningful value
 
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
+        # #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
+        # #  autocompletion
+        self.controller: ActuatorWrapper
 
         #TODO declare here attributes you want/need to init with a default value
+
         pass
 
     def get_actuator_value(self):
@@ -68,30 +79,16 @@ class DAQ_Move_Template(DAQ_Move_base):
         float: The position obtained after scaling conversion.
         """
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        pos = DataActuator(data=self.controller.your_method_to_get_the_actuator_value())  # when writing your own plugin replace this line
+        # raise NotImplemented  # when writing your own plugin remove this line
+        pos = DataActuator(data=self.controller.get_pos())  # when writing your own plugin replace this line
         pos = self.get_position_with_scaling(pos)
         return pos
 
-    def user_condition_to_reach_target(self) -> bool:
-        """ Implement a condition for exiting the polling mechanism and specifying that the
-        target value has been reached
-
-       Returns
-        -------
-        bool: if True, PyMoDAQ considers the target value has been reached
-        """
-        # TODO either delete this method if the usual polling is fine with you, but if need you can
-        #  add here some other condition to be fullfilled either a completely new one or
-        #  using or/and operations between the epsilon_bool and some other custom booleans
-        #  for a usage example see DAQ_Move_brushlessMotor from the Thorlabs plugin
-        return True
-
     def close(self):
         """Terminate the communication protocol"""
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
+        # ## TODO for your custom plugin
+        # raise NotImplemented  # when writing your own plugin remove this line
+        # #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -103,13 +100,48 @@ class DAQ_Move_Template(DAQ_Move_base):
         """
         ## TODO for your custom plugin
         if param.name() == 'axis':
-            self.axis_unit = self.controller.your_method_to_get_correct_axis_unit()
+            self.controller.set_axis(self.axis_name)
+            pos = self.get_actuator_value()
+            if self.axis_name == "Delay":
+                self.controller.set_burst(mode="ON")
+            self.axis_unit = self.controller.get_unit()
             # do this only if you can and if the units are not known beforehand, for instance
             # if the motors connected to the controller are of different type (mm, µm, nm, , etc...)
             # see BrushlessDCMotor from the thorlabs plugin for an exemple
 
-        elif param.name() == "a_parameter_you've_added_in_self.params":
-           self.controller.your_method_to_apply_this_param_change()
+        elif param.name() == "burst":
+            self.controller.set_burst(mode=param.value())
+        elif param.name() == "frequency":
+            self.controller.set_frequency(param.value())
+        elif param.name() == "offset":
+            self.controller.set_offset(param.value())
+        elif param.name() == "delay":
+            self.controller.set_delay(param.value())
+        elif param.name() == "cycles":
+            self.controller.set_cycles(param.value())
+        elif param.name() == "wavetype":
+            wavetype = param.value().get('selected')[0]
+            self.controller.set_wavetype(wavetype)
+
+            burst_state = self.controller.get_burst_state()
+            self.settings.child('burst').setValue(burst_state)
+
+            dc = (wavetype == "DC")
+            offset = self.controller.get_offset(DC=dc)
+            self.settings.child('offset').setValue(offset)
+        elif param.name() == "file":
+            self.controller.set_arbwave(param.value())
+            self.settings.child('wavetype').setValue(
+                dict(all_items=["ARB", "SINE", "RAMP", "SQUARE", "DC"], selected=["ARB"]))
+
+            freq = self.controller.get_frequency()
+            self.settings.child('frequency').setValue(freq)
+
+            offset = self.controller.get_offset()
+            self.settings.child('offset').setValue(offset)
+
+            burst_state = self.controller.get_burst_state()
+            self.settings.child('burst').setValue(burst_state)
         else:
             pass
 
@@ -127,16 +159,16 @@ class DAQ_Move_Template(DAQ_Move_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        raise NotImplemented  # TODO when writing your own plugin remove this line and modify the ones below
-        self.ini_stage_init(slave_controller=controller)  # will be useful when controller is slave
-
+        ##### Si slave ya moyen que ça foire
         if self.is_master:  # is needed when controller is master
-            self.controller = PythonWrapperOfYourInstrument(arg1, arg2, ...) #  arguments for instantiation!)
+            self.controller = ActuatorWrapper()
             # todo: enter here whatever is needed for your controller initialization and eventual
             #  opening of the communication channel
 
-        info = "Whatever info you want to log"
-        initialized = self.controller.a_method_or_atttribute_to_check_if_init()  # todo
+        self.controller.__init__()  # todo
+        info = "siglent connected"
+        initialized = self.controller.open_communication()
+
         return info, initialized
 
     def move_abs(self, value: DataActuator):
@@ -151,9 +183,9 @@ class DAQ_Move_Template(DAQ_Move_base):
         self.target_value = value
         value = self.set_position_with_scaling(value)  # apply scaling if the user specified one
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_an_absolute_value(value.value())  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_pos(value.value())  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand('Update_Status', ['position updated']))
 
     def move_rel(self, value: DataActuator):
         """ Move the actuator to the relative target actuator value defined by value
@@ -162,30 +194,36 @@ class DAQ_Move_Template(DAQ_Move_base):
         ----------
         value: (float) value of the relative target positioning
         """
+        pos = self.controller.get_pos()
+        self.current_position = pos
+
         value = self.check_bound(self.current_position + value) - self.current_position
         self.target_value = value + self.current_position
         value = self.set_position_relative_with_scaling(value)
 
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_a_relative_value(value.value())  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        # self.controller.set_rel_pos(value.value())  # when writing your own plugin replace this line
+
+        self.controller.set_pos(pos + value.value())
+        self.emit_status(ThreadCommand('Update_Status', ['position updated']))
 
     def move_home(self):
         """Call the reference method of the controller"""
 
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_get_to_a_known_reference()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_amplitude(amp=2)  # when writing your own plugin replace this line
+        self.controller.set_phase(phi=0)
+        self.emit_status(ThreadCommand('Update_Status', ['moved home (amp = 2, phi=0)']))
 
     def stop_motion(self):
       """Stop the actuator and emits move_done signal"""
 
       ## TODO for your custom plugin
-      raise NotImplemented  # when writing your own plugin remove this line
-      self.controller.your_method_to_stop_positioning()  # when writing your own plugin replace this line
-      self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+    #   raise NotImplemented  # when writing your own plugin remove this line
+      self.controller.set_state("OFF")  # when writing your own plugin replace this line
+      self.emit_status(ThreadCommand('Update_Status', ['channel output OFF']))
 
 
 if __name__ == '__main__':
---
+++ src/pymodaq_plugins_siglent/daq_move_plugins/siglent_wrapper.py
@@ -0,0 +1,282 @@
+import pyvisa
+import time as t
+
+serial_address = 'USB0::0xF4EC::0x1102::SDG7ABAD7R0103::INSTR'
+rm = pyvisa.ResourceManager()
+global siglent
+siglent = rm.open_resource(serial_address)
+
+class ActuatorWrapper:
+    """
+    Attributes:
+    -----------
+    burst: string
+        Burst operating mode, either "ON" or "OFF"
+
+    load: string
+        Impedance load, "50" to "100000" Ohm or "HiZ"
+
+    amplitude: float
+        Amplitude of the signal, in V
+    
+    offset: float
+        Offset of the signal, in V
+
+    phase: float
+        Phase of the signal, in degrees °
+
+    frequency: float   
+        Frequency of the signal, in Hz
+
+    cycles: int
+        Number of repetition of the burst original signal, only available in "BURST" mode
+
+    trig_src: string
+        Trigger source, either "EXT", "INT" or "MAN", only available in "BURST" mode
+
+    delay: float
+        Trigger delay, in s (min value is 2.494us and it is not stable when set too high (like 1ms))
+    
+    wavetype: string
+        Wavetype of the signal, either "SINE", "SQUARE", "RAMP", "DC" or "ARB"
+        "ARB" as in arbitrary waveform which is sent via EasyWaveX and cannot originate from here
+
+    file: string
+        wavefile name, registered locally and available in "ARB" mode.
+
+    axis: string
+        Parameter on which the scans will be performed, either "Amplitude"
+        or "Phase" for now
+
+    channel: string
+        The output channel, "CH1" by default
+
+    state: string
+        Either "ON" or "OFF"
+    """
+
+    # units = 'V or °'
+    global siglent
+
+    def __init__(self):
+
+        siglent.write("C1:OUTP LOAD,50")
+        siglent.write("C1:BTWV STATE,ON")
+        siglent.write("C1:BTWV TRSR,EXT")
+        siglent.write("C1:BSWV WVTP,SINE")
+        siglent.write("C1:BSWV FRQ,10000000")
+        siglent.write("C1:BSWV AMP,3")
+        siglent.write("C1:BSWV PHSE,0")
+        siglent.write("C1:BTWV DLAY,0.000005")
+        siglent.write("C1:BTWV TIME,1")
+        siglent.write("C1:OUTP ON")
+
+        self.address = serial_address
+        self.burst = "ON"
+        self.amplitude = 3
+        self.offset = 0
+        self.phase = 0
+        self.frequency = 10000000
+        self.delay = 0.000005
+        self.cycles = 1
+        self.trig_src = "EXT"
+        self.wavetype = "SINE"
+        self.axis = "Amplitude"
+        self.channel = "C1"
+        self.state = "OFF"
+        self.unit = 'V'
+
+        # self.set_rel_amplitude(1)
+        # self.set_rel_amplitude(-1)
+
+    def open_communication(self):
+        return True
+    
+    def get_amplitude(self):
+        answer = siglent.query(self.channel + ":BSWV?")
+        amplitude = answer.split(',')[7]    # The 7th word corresponds to the amp
+        amplitude = float(amplitude[:-1])   # We skip the 'V' at the end
+        print("amplitude =", amplitude)
+        self.amplitude = amplitude
+        return(self.amplitude)
+
+    
+    def get_phase(self):
+        answer = siglent.query(self.channel + ":BSWV?")
+        wtype = self.get_wavetype()
+        if (wtype == "RAMP") or (wtype == "SQUARE"):
+            phase = answer.split(',')[-3]   # The 3rd to last word corresponds to the phase
+            phase = float(phase)
+        else:
+            phase = answer.split(',')[-1]   # The last word corresponds to the phase
+            phase = float(phase[:-1])       # We get rid of the '\n' at the end
+        self.phase = phase
+        print("phase =", self.phase)
+        return(self.phase)
+
+    def set_burst(self, mode):
+        """Sets the burst parameter "ON" or "OFF" """
+        siglent.write(self.channel + ":BTWV STATE," + mode)
+        self.mode = mode
+
+    def get_burst_state(self):
+        answer = siglent.query(self.channel + ":BTWV?")
+        state = answer.split(',')[1]
+        self.burst = state
+        return(self.burst)
+    
+    def set_amplitude(self, amp):
+        """Sets the amplitude to amp, in V"""
+        siglent.write(self.channel + ":BSWV AMP," + str(amp))
+        self.amplitude = amp
+
+    def set_rel_amplitude(self, rel_amp):
+        """Sets the amplitude to amp + rel_amp, in V"""
+        amp = self.get_amplitude()
+        # siglent.write(self.channel + ":BSWV AMP," + str(amp + rel_amp))
+        new_amp = amp + rel_amp
+        self.set_amplitude(new_amp)
+        self.amplitude = new_amp
+        return
+
+    def set_phase(self, phi):
+        """Sets the phase to phi, in degrees"""
+        siglent.write(self.channel + ":BSWV PHSE," + str(phi))
+        self.phase = phi
+        return
+
+    def set_rel_phase(self, rel_phi):
+        """Sets the phase to phi + rel_phi, in degrees"""
+        phi = self.get_phase
+        siglent.write(self.channel + ":BSWV PHSE," + str(phi + rel_phi))
+        self.phase = phi + rel_phi
+
+    def set_offset(self, ofst):
+        """Sets the amplitude offset to ofst, in V"""
+        siglent.write(self.channel + ":BSWV OFST," + str(ofst))
+        self.offset = ofst
+
+    def get_offset(self, DC=False):
+        answer = siglent.query("C1:BSWV?")
+        if DC:
+            ofst = answer.split(',')[7]
+            ofst = float(ofst[:-2])         # We skip the 'V' & \n at the end
+        else:
+            ofst = answer.split(',')[17]    # the 18th word corresponds to the ofst
+            ofst = float(ofst[:-1])         # We skip the 'V' at the end
+        self.offset = ofst
+        return self.offset
+
+    def set_axis(self, axis):
+        self.axis = axis
+
+    def set_unit(self):
+        axis = self.axis
+        if axis == 'Amplitude':
+            self.unit = 'V'
+        elif axis == 'Phase':
+            self.unit = 'deg'
+        elif axis == 'Frequency':
+            self.unit = 'Hz'
+        else:  # axis == 'Delay'
+            self.unit = 's'
+
+    def get_unit(self):
+        unit = self.unit
+        return unit
+
+    def set_pos(self, pos):
+        if self.axis == "Amplitude":
+            return(self.set_amplitude(pos))
+        elif self.axis == "Phase":
+            return(self.set_phase(pos))
+        elif self.axis == "Frequency":
+            return(self.set_frequency(pos))
+
+    def get_pos(self):
+        if self.axis == "Amplitude":
+            return(self.get_amplitude())
+        elif self.axis == "Phase":
+            return(self.get_phase())
+        elif self.axis == "Frequency":
+            return(self.get_frequency())
+
+    def set_rel_pos(self, pos):
+        if self.axis == "Amplitude":
+            return(self.set_rel_amplitude(pos))
+        elif self.axis == "Phase":
+            return(self.set_rel_phase(pos))
+    
+    def set_wavetype(self, wtype):
+        self.wavetype = wtype
+        siglent.write(self.channel + ":BSWV WVTP," + wtype)
+
+    def get_wavetype(self):
+        return(self.wavetype)
+    
+    def set_load(self, load):
+        """Sets the load to "HiZ" (ie High Z) or "50" to "100000" Ohm """
+        siglent.write(self.channel + ":OUTP LOAD," + load)
+        self.load = load
+
+    def get_load(self):
+        return(self.load)
+    
+    def set_trig_src(self, source):
+        """Sets the trigger source, either "EXT", "INT" or "MAN"
+            only available when burst mode is on"""
+        siglent.write(self.channel + ":BTWV TRSR," + source)
+        self.trig_src = source
+    
+    def get_trig_src(self):
+        return(self.trig_src)
+    
+    def set_frequency(self, freq):
+        """sets the basewave frequency, in Hz"""
+        siglent.write(self.channel + ":BSWV FRQ," + str(freq))
+        self.frequency = freq
+    
+    def get_frequency(self):
+        answer = siglent.query("C1:BSWV?")
+        freq = answer.split(",")[3]
+        freq = float(freq[:-2])         # get rid of the "HZ" at the end
+        self.frequency = freq
+        return(self.frequency)
+    
+    def set_delay(self, time):
+        """Sets the delay, in s, only available in BTWV ext trigger mode"""
+        siglent.write(self.channel + ":BTWV DLAY," + str(time))
+        self.delay = time
+
+    def get_delay(self):
+        return(self.delay)
+    
+    def set_cycles(self, n):
+        """Sets the number of repetition of the basewave in the burst, int"""
+        siglent.write(self.channel + ":BTWV TIME," + str(n))
+        self.cycles = n
+
+    def get_cycles(self):
+        return(self.cycles)
+    
+    def set_arbwave(self, file):
+        """Sets the arbitrary wavefile to be displayed by the siglent module"""
+        self.set_wavetype("ARB")
+        siglent.write(self.channel + f':ARWV NAME,"{file}"')
+        # mandatory if we then want to change signal characteristics such as frequency
+        siglent.write(self.channel + ":ARBM AFG")
+        self.file = file
+        # Actualise class values
+        amplitude = self.get_amplitude()
+        phase = self.get_phase()
+
+    def get_file(self):
+        return(self.file)
+    
+    def set_state(self, state):
+        """Sets the channel output "ON" or "OFF" """
+        siglent.write(self.channel + ":OUTP " + state)
+        self.state = state
+
+    def get_state(self):
+        return(self.state)
